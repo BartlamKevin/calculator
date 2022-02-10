937a91638725df8199e4d7a804a70e5f6be85cc0
nevik <bartlamkevin@gmail.com>
2022-02-10 14:14:02 -0500

    all done

--- calculator-test.js
+++ calculator-test.js
@@ -1,23 +1,33 @@
 
-it('should calculate the monthly rate correctly', function () {
+it("should calculate the monthly rate correctly", function () {
   // ...
   const values ={
     amount: 9001,
     years: 7,
     rate:12
   };
-  expect(calculateMonthlyPayment.(values).toEqual(158.89));
+  expect(calculateMonthlyPayment(values)).toEqual("158.89");
 });
 
 
-it("should return a result with 2 decimal places", function() {
+it("should return payment amount with two zeros", function() {
   // ..
   const values ={
-    amount: 12000,
-    years: 10,
-    rate: 5.5
+    amount: 20099,
+    years: 16,
+    rate: 2.8
   };
-  expect(calculateMonthlyPayment.(values).toEqual(130.23));
+  expect(calculateMonthlyPayment(values)).toEqual("130.00");
+});
+
+it("should return a payment amount with high interset and two zeros", function() {
+  // ..
+  const values ={
+    amount: 1000,
+    years: 1,
+    rate: 100
+  };
+  expect(calculateMonthlyPayment(values)).toEqual("135.00");
 });
 
 /// etc
